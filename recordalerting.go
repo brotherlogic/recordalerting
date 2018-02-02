2fb85baa75d76d6f23b7a0f312ba61fd6166dfae
Simon Tucker <brotherlogic@gmail.com>
2018-02-01 19:06:43 -0800

    Adds github scaffold. This closes #3

--- recordalerting.go
+++ recordalerting.go
@@ -1,15 +1,48 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
+	"time"
 
 	"github.com/brotherlogic/goserver"
 	"google.golang.org/grpc"
 
+	pbgh "github.com/brotherlogic/githubcard/proto"
 	pbg "github.com/brotherlogic/goserver/proto"
+	"github.com/brotherlogic/goserver/utils"
+	pbrc "github.com/brotherlogic/recordcollection/proto"
 )
+
+type gh interface {
+	alert(r *pbrc.Record, text string) error
+}
+
+type prodGh struct{}
+
+func (gh *prodGh) recordMissing(r *pbrc.Record, text string) error {
+	host, port, err := utils.Resolve("githubcard")
+
+	if err != nil {
+		return err
+	}
+
+	conn, err := grpc.Dial(host+":"+strconv.Itoa(int(port)), grpc.WithInsecure())
+	defer conn.Close()
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	client := pbgh.NewGithubClient(conn)
+	_, err = client.AddIssue(ctx, &pbgh.Issue{Title: "Problematic Record", Body: fmt.Sprintf("%v - %v", text, r.GetRelease().Title), Service: "recordcollection"})
+	return err
+}
 
 //Server main server type
 type Server struct {
