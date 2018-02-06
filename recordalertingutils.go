faabe986ed8a3a3c77805d30c66a98d1a5563e69
Simon Tucker <brotherlogic@gmail.com>
2018-02-05 17:10:15 -0800

    Only alert on clean records

--- recordalertingutils.go
+++ recordalertingutils.go
@@ -6,7 +6,12 @@ func (s *Server) alertForPurgatory() {
 	records, err := s.rc.getRecordsInPurgatory()
 	if err == nil {
 		if len(records) > 0 {
-			s.gh.alert(records[0], fmt.Sprintf("%v is in Purgatory!", records[0].GetRelease().Title))
+			for _, r := range records {
+				if !r.GetMetadata().GetDirty() {
+					s.gh.alert(records[0], fmt.Sprintf("%v is in Purgatory!", records[0].GetRelease().Title))
+					break
+				}
+			}
 		}
 	}
 }
