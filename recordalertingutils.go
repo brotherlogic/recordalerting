aa3ba4df0f84cd006c53a1f1b7538a397b6a8c8a
simon <brotherlogic@gmail.com>
2021-08-28 18:25:50 -0700

    Fix update time on sale price:

--- recordalertingutils.go
+++ recordalertingutils.go
@@ -109,7 +109,7 @@ func (s *Server) alertForMissingSaleID(r *pbrc.Record) {
 	}
 
 	//Also validate the current sale price
-	if r.GetMetadata().GetSalePriceUpdate() > r.GetMetadata().GetDateUpdated() && r.GetRelease().GetFolderId() == 812802 {
+	if r.GetMetadata().GetSalePriceUpdate() != 0 && time.Since(time.Unix(r.GetMetadata().GetSalePriceUpdate(), 0)) > time.Hour*24*30 && r.GetRelease().GetFolderId() == 812802 {
 		s.alertCount++
 		s.RaiseIssue(fmt.Sprintf("%v is a problematic record price", r.GetRelease().GetInstanceId()), fmt.Sprintf("%v (%v) has not had a sale price update since %v", r.GetRelease().GetId(), r.GetRelease().GetInstanceId(), time.Unix(r.GetMetadata().GetSalePriceUpdate(), 0)))
 	}
