816090a826b376456da7b98349868aedd090279a
simon <brotherlogic@gmail.com>
2021-09-07 18:04:58 -0700

    Skip cleaning requirements for unknown records

--- recordalertingutils.go
+++ recordalertingutils.go
@@ -97,11 +97,13 @@ func (s *Server) assessRecord(ctx context.Context, config *pb.Config, r *pbrc.Re
 	err4 := s.needsCondition(ctx, config, r)
 
 	// Only fail
-	if (r.GetMetadata().GetFiledUnder() == pbrc.ReleaseMetadata_FILE_12_INCH || r.GetMetadata().GetFiledUnder() == pbrc.ReleaseMetadata_FILE_7_INCH) && r.GetMetadata().GetSaleState() != gd.SaleState_SOLD {
-		if time.Since(time.Unix(r.GetMetadata().GetLastCleanDate(), 0)) > time.Hour*24*365*3 {
-			err := s.rc.clean(ctx, r.GetRelease().GetInstanceId())
-			if err != nil {
-				return err
+	if r.GetMetadata().GetCategory() != pbrc.ReleaseMetadata_UNKNOWN {
+		if (r.GetMetadata().GetFiledUnder() == pbrc.ReleaseMetadata_FILE_12_INCH || r.GetMetadata().GetFiledUnder() == pbrc.ReleaseMetadata_FILE_7_INCH) && r.GetMetadata().GetSaleState() != gd.SaleState_SOLD {
+			if time.Since(time.Unix(r.GetMetadata().GetLastCleanDate(), 0)) > time.Hour*24*365*3 {
+				err := s.rc.clean(ctx, r.GetRelease().GetInstanceId())
+				if err != nil {
+					return err
+				}
 			}
 		}
 	}
