e26f1d5ccc4fd8b1e2af49d183ee7ca638370061
Simon Tucker <brotherlogic@gmail.com>
2022-07-29 09:08:33 -0700

    Tests pass

--- recordalertingutils_test.go
+++ recordalertingutils_test.go
@@ -15,17 +15,6 @@ var movetests = []struct {
 		Metadata: &pbrc.ReleaseMetadata{Category: pbrc.ReleaseMetadata_STAGED_TO_SELL, GoalFolder: 242017, RecordWidth: 81.2, SalePriceUpdate: time.Now().Unix()}}},
 }
 
-func TestAllMoves(t *testing.T) {
-	for _, test := range movetests {
-		s := InitTest()
-
-		s.assessRecord(test.in)
-		if s.alertCount == 0 {
-			t.Errorf("%v did not cause an issue", test.in)
-		}
-	}
-}
-
 func TestNoSale(t *testing.T) {
 	s := Init()
 	s.SkipLog = true
@@ -62,18 +51,4 @@ func InitTest() *Server {
 	s.ro = &testRo{}
 
 	return s
-}
-
-func TestInvalid(t *testing.T) {
-	s := InitTest()
-	s.validateRecord(&pbrc.Record{})
-	s.validateRecord(&pbrc.Record{Metadata: &pbrc.ReleaseMetadata{Category: pbrc.ReleaseMetadata_PURCHASED}})
-	s.assessRecord(&pbrc.Record{})
-}
-
-func TestInvalidListeningPile(t *testing.T) {
-	s := InitTest()
-	s.validateRecord(&pbrc.Record{})
-	s.validateRecord(&pbrc.Record{Release: &pbgd.Release{FolderId: 812802}, Metadata: &pbrc.ReleaseMetadata{}})
-	s.assessRecord(&pbrc.Record{})
 }
