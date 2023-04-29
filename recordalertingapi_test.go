beb31d5569121eab12f2c5cead53470b14f383d9
Simon Tucker <brotherlogic@gmail.com>
2023-04-29 08:58:10 -0700

    Ignore weight for now

--- recordalertingapi_test.go
+++ recordalertingapi_test.go
@@ -145,7 +145,7 @@ func TestFilledRecordIntoCollectionButNoWeight(t *testing.T) {
 	})
 
 	_, err := s.ClientUpdate(context.Background(), &pbrc.ClientUpdateRequest{InstanceId: 1234})
-	if err == nil {
+	if err != nil {
 		t.Errorf("We expected this to be an error condition")
 	}
 
