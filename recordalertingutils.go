62274bbc9cdc31467ac85f8fc6a7707502571386
Simon Tucker <brotherlogic@gmail.com>
2023-04-30 20:13:06 -0700

    Only require keeper on records older than one year

--- recordalertingutils.go
+++ recordalertingutils.go
@@ -135,7 +135,7 @@ func (s *Server) needsSold(ctx context.Context, config *pb.Config, r *pbrc.Recor
 
 func (s *Server) needsKeeperJudgement(ctx context.Context, config *pb.Config, r *pbrc.Record) error {
 	return s.adjustState(ctx, config, r,
-		r.GetMetadata().GetFiledUnder() != pbrc.ReleaseMetadata_FILE_DIGITAL && r.GetRelease().GetFolderId() == 812802 && r.GetMetadata().GetCategory() == pbrc.ReleaseMetadata_IN_COLLECTION && r.GetMetadata().GetKeep() == pbrc.ReleaseMetadata_KEEP_UNKNOWN, pb.Problem_NEEDS_KEEPER, "needs keeper judgement")
+		time.Since(time.Unix(r.GetMetadata().GetDateAdded(), 0)) > time.Hour*24*265 && r.GetMetadata().GetFiledUnder() != pbrc.ReleaseMetadata_FILE_DIGITAL && r.GetRelease().GetFolderId() == 812802 && r.GetMetadata().GetCategory() == pbrc.ReleaseMetadata_IN_COLLECTION && r.GetMetadata().GetKeep() == pbrc.ReleaseMetadata_KEEP_UNKNOWN, pb.Problem_NEEDS_KEEPER, "needs keeper judgement")
 }
 
 func (s *Server) needsDigitalAssess(ctx context.Context, config *pb.Config, r *pbrc.Record) error {
