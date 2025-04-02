6eee32cd3eadd080a79646552f36fbb8c2ec388e
Simon Tucker <brotherlogic@gmail.com>
2025-04-02 10:46:45 -0700

    Track width of 45s

--- recordalertingutils.go
+++ recordalertingutils.go
@@ -98,7 +98,7 @@ func (s *Server) staleLimbo(ctx context.Context, config *pb.Config, r *pbrc.Reco
 
 func (s *Server) needsWidth(ctx context.Context, config *pb.Config, r *pbrc.Record) error {
 	return s.adjustState(ctx, config, r,
-		(r.GetRelease().GetFolderId() == 812802 || r.GetRelease().GetFolderId() == 7651472) &&
+		(r.GetRelease().GetFolderId() == 7665013 || r.GetRelease().GetFolderId() == 812802 || r.GetRelease().GetFolderId() == 7651472) &&
 			r.GetMetadata().GetFiledUnder() != pbrc.ReleaseMetadata_FILE_DIGITAL &&
 			r.GetMetadata().GetFiledUnder() != pbrc.ReleaseMetadata_FILE_UNKNOWN &&
 			r.GetMetadata().GetRecordWidth() <= 0.5 && r.GetMetadata().GetCategory() != pbrc.ReleaseMetadata_PURCHASED,
