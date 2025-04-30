8b7f527cc708baaa25953af73b53528a91899a96
Simon Tucker <brotherlogic@gmail.com>
2025-04-30 08:49:29 -0700

    Ensure we get weight on STS items

--- recordalertingutils.go
+++ recordalertingutils.go
@@ -86,7 +86,7 @@ func (s *Server) adjustState(ctx context.Context, config *pb.Config, r *pbrc.Rec
 
 func (s *Server) needsWeight(ctx context.Context, config *pb.Config, r *pbrc.Record) error {
 	return s.adjustState(ctx, config, r,
-		((r.GetMetadata().GetSetRating() > 0 || r.GetRelease().GetRating() > 0) && r.GetMetadata().GetFiledUnder() != pbrc.ReleaseMetadata_FILE_DIGITAL && (r.GetRelease().GetFolderId() == 812802) && r.GetMetadata().GetWeightInGrams() <= 5),
+		(r.GetMetadata().GetCategory() == pbrc.ReleaseMetadata_STAGED_TO_SELL && r.GetRelease().GetFolderId() == 7651472 && r.GetMetadata().GetWeightInGrams() <= 5),
 		pb.Problem_MISSING_WEIGHT, "needs weight")
 }
 
